edea73abf38c584d9eaa87cac77598d6d51cc8d7
Thiago Freitas <thiagoleonardo1995@live.com>
2023-06-27 23:14:42 -0300

    Primeiros testes de conexão com o back-end

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -1,21 +1,30 @@
-import './styles.css';
-import SearchBar from '../../../components/SearchBar';
-import CatalogCard from '../../../components/CatalogCard';
+import { useEffect, useState } from 'react';
 import ButtonNextPage from '../../../components/ButtonNextPage';
+import CatalogCard from '../../../components/CatalogCard';
+import SearchBar from '../../../components/SearchBar';
+import { ProductDTO } from '../../../models/product';
 import * as productService from '../../../services/product-service';
+import './styles.css';
 
 export default function Catalog() {
-    return (
 
+    const [products, setProducts] = useState<ProductDTO[]>([]);
+
+    useEffect(() => {
+        productService.findAll().then(response => {
+            setProducts(response.data.content);
+        });
+    }, []);
+
+    return (
         <main>
             <section id="catalog-section" className="dsc-container">
                 <SearchBar />
 
                 <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
-
-                    { productService.findAll().map(
-                        product => <CatalogCard key={product.id} product={product} />
-                    )}
+                    {
+                        products.map(product => <CatalogCard key={product.id} product={product} />)
+                    }
                 </div>
                 <ButtonNextPage />
             </section>
