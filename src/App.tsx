e41a774c97bdd81a589f5d7f8855a592b48257c8
Thiago Freitas <thiagoleonardo1995@live.com>
2023-07-21 04:29:18 -0300

    Admin area routes, Product CRUD static layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,19 @@ import { PrivateRoute } from './components/PrivateRoute';
 import { AccessTokenPayloadDTO } from './models/auth';
 import Admin from './routes/Admin';
 import AdminHome from './routes/Admin/AdminHome';
-import Login from './routes/ClientHome/Login';
+import ProductForm from './routes/Admin/ProductForm';
+import ProductListing from './routes/Admin/ProductListing';
 import ClientHome from './routes/ClientHome';
 import Cart from './routes/ClientHome/Cart';
 import Catalog from './routes/ClientHome/Catalog';
+import Confirmation from './routes/ClientHome/Confirmation';
+import Login from './routes/ClientHome/Login';
 import ProductDetails from './routes/ClientHome/ProductDetails';
 import * as authService from './services/auth-service';
 import * as cartService from './services/cart-service';
 import { ContextCartCount } from './utils/context-cart';
 import { ContextToken } from './utils/context-token';
 import { history } from './utils/history';
-import Confirmation from './routes/ClientHome/Confirmation';
 
 export default function App() {
 
@@ -45,7 +47,10 @@ export default function App() {
               <Route path="confirmation/:orderId" element={<PrivateRoute><Confirmation /></PrivateRoute>} />
             </Route>
             <Route path="/admin/" element={<PrivateRoute roles={['ROLE_ADMIN']}><Admin /></PrivateRoute>}>
-              <Route index element={<AdminHome />} />
+              <Route index element={<Navigate to='/admin/home' />} />
+              <Route path='home' element={<AdminHome />} />
+              <Route path="products" element={<ProductListing />} />
+              <Route path="products/:productId" element={<ProductForm />} />
             </Route>
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
