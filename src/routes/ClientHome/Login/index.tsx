806ae729d4883c309ccd6bdc938969c7cdfe69e9
Thiago Freitas <thiagoleonardo1995@live.com>
2023-07-25 04:59:35 -0300

    Forms Update

--- src/routes/ClientHome/Login/index.tsx
+++ src/routes/ClientHome/Login/index.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import FormInput from "../../../components/FormImput";
 import * as authService from '../../../services/auth-service';
 import { ContextToken } from "../../../utils/context-token";
+import * as forms from '../../../utils/forms';
 import './styles.css';
 
 export default function Login() {
@@ -49,7 +50,7 @@ export default function Login() {
     function handleImputChange(event: any) {
         const value = event?.target.value;
         const name = event?.target.name;
-        setFormData({ ...formData, [name]: { ...formData[name], value: value } });
+        setFormData(forms.update(formData, name, value));
     }
 
     return (
