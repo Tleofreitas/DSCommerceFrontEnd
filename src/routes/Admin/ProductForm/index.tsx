ceb299fcde6b7faf06c59be1aa953eecb92f5f5f
Thiago Freitas <thiagoleonardo1995@live.com>
2023-10-02 13:46:17 -0300

    forms toDirty, dirty behaviour

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -38,6 +38,8 @@ export default function ProductForm() {
     });
 
     useEffect(() => {
+        const result = forms.toDirty(formData, "price");
+        console.log(result);
         if (isEditing) {
             productService.findById(Number(params.productId))
                 .then(response => {
@@ -55,6 +57,11 @@ export default function ProductForm() {
         setFormData(dataValidated);
     }
 
+    function handleInputTurnDirty(name: string) {
+        const newFormData = forms.toDirty(formData, name);
+        setFormData(newFormData);
+    }
+
     return (
         <main>
             <section id="product-form-section" className="dsc-container">
@@ -67,6 +74,7 @@ export default function ProductForm() {
                                     {...formData.name}
                                     className="dsc-form-control"
                                     onChange={handleInputChange}
+                                    onTurnDirty={handleInputTurnDirty}
                                 />
                                 <div className='dsc-form-error'>
                                     {formData.name.message}
@@ -77,6 +85,7 @@ export default function ProductForm() {
                                     {...formData.price}
                                     className="dsc-form-control"
                                     onChange={handleInputChange}
+                                    onTurnDirty={handleInputTurnDirty}
                                 />
                                 <div className='dsc-form-error'>
                                     {formData.price.message}
@@ -87,6 +96,7 @@ export default function ProductForm() {
                                     {...formData.imgUrl}
                                     className="dsc-form-control"
                                     onChange={handleInputChange}
+                                    onTurnDirty={handleInputTurnDirty}
                                 />
                             </div>
                         </div>
