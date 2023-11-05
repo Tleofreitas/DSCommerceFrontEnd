dba0c09f8673f0c8090621d11bc848865bc5cf8b
Thiago Freitas <thiagoleonardo1995@live.com>
2023-11-05 13:27:50 -0300

    Ajuste de login

--- FrontEnd/src/services/auth-service.ts
+++ FrontEnd/src/services/auth-service.ts
@@ -17,7 +17,7 @@ export function loginRequest(loginData: CredentialsDTO) {
 
     const config: AxiosRequestConfig = {
         method: "POST",
-        url: "/oauth/token",
+        url: "/oauth2/token",
         data: requestBody,
         headers
     }
